e31284dabb3f63ca709cf2ca8e38dd4069ae4517
fpcoutinho <fpcoutinho5@gmail.com>
2023-05-10 16:11:35 -0300

    partials

--- app.js
+++ app.js
@@ -10,14 +10,34 @@ console.log(`Server running at http://localhost:8080/`);
 
 //Routes
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", { titulo: "Início" });
 });
 app.get("/sobre", (req, res) => {
-  res.render("about");
+  res.render("about", { titulo: "Sobre", nome: "Filipe" });
 });
 app.get("/contato", (req, res) => {
-  res.render("contact");
+  res.render("contact", { titulo: "Contato" });
+});
+app.get("/blogs", (req, res) => {
+  const blogs = [
+    {
+      title: "Yoshi finds eggs",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "Mario finds stars",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+    {
+      title: "How to defeat bowser",
+      snippet: "Lorem ipsum dolor sit amet consectetur",
+    },
+  ];
+  res.render("blogs", { titulo: "Blogs", blogs });
+});
+app.get("/blogs/novo", (req, res) => {
+  res.render("create", { titulo: "Criar novo Post" });
 });
 app.use((req, res) => {
-  res.status(404).render("404");
+  res.status(404).render("404", { titulo: "404" });
 });
