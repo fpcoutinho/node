53a00596fd6157b194e80c57245c32c07c6ef3a1
fpcoutinho <fpcoutinho5@gmail.com>
2023-05-10 22:00:56 -0300

    POST funcionando

--- app.js
+++ app.js
@@ -1,21 +1,74 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 const app = express();
-//View Engines
-app.set("view engine", "ejs");
 
-//Server
-app.listen(8080);
-console.log(`Server running at http://localhost:8080/`);
+//Conexão ao MongoDB
+const uri =
+  "mongodb+srv://filcoutinho:mwYtV1kwGm4nX61N@nodetutorial.ojtsrqk.mongodb.net/node-tuts?retryWrites=true&w=majority";
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("Successful connection to the Database");
+    //Server
+    app.listen(8080);
+    console.log(`Server running at http://localhost:8080/`);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+//View Engine ejs
+app.set("view engine", "ejs");
 
 //Middleware e Static files
 app.use(morgan("dev"));
 app.use(express.static("static"));
+app.use(express.urlencoded({ extended: true }));
 
-//Routes
+//Rotas de mongoose e mongo sandbox
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "Novo Blog",
+    snippet: "Sobre o meu novo blog",
+    body: "Mais sobre o meu novo blog",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//Rotas padrão
 app.get("/", (req, res) => {
-  res.render("index", { titulo: "Início" });
+  res.redirect("/blogs");
 });
 app.get("/sobre", (req, res) => {
   res.render("about", { titulo: "Sobre", nome: "Filipe" });
@@ -23,26 +76,35 @@ app.get("/sobre", (req, res) => {
 app.get("/contato", (req, res) => {
   res.render("contact", { titulo: "Contato" });
 });
+
+//Rotas de blogs
 app.get("/blogs", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-  res.render("blogs", { titulo: "Blogs", blogs });
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("blogs", { titulo: "Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 app.get("/novo", (req, res) => {
   res.render("create", { titulo: "Criar novo Post" });
 });
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  console.log(blog);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//Rota 404
 app.use((req, res) => {
   res.status(404).render("404", { titulo: "404" });
 });
