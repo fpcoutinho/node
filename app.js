81c9becc899f48a92a6d0c7a477f02db06da485c
fpcoutinho <fpcoutinho5@gmail.com>
2023-05-11 09:03:05 -0300

    routers

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogRoutes = require("./routes/blog-routes");
 
 const app = express();
 
@@ -28,44 +28,6 @@ app.use(morgan("dev"));
 app.use(express.static("static"));
 app.use(express.urlencoded({ extended: true }));
 
-//Rotas de mongoose e mongo sandbox
-app.get("/add-blog", (req, res) => {
-  const blog = new Blog({
-    title: "Novo Blog",
-    snippet: "Sobre o meu novo blog",
-    body: "Mais sobre o meu novo blog",
-  });
-  blog
-    .save()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/all-blogs", (req, res) => {
-  Blog.find()
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
 //Rotas padrão
 app.get("/", (req, res) => {
   res.redirect("/blogs");
@@ -78,31 +40,7 @@ app.get("/contato", (req, res) => {
 });
 
 //Rotas de blogs
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      res.render("blogs", { titulo: "Blogs", blogs: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-app.get("/novo", (req, res) => {
-  res.render("create", { titulo: "Criar novo Post" });
-});
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-  console.log(blog);
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+app.use("/blogs", blogRoutes);
 
 //Rota 404
 app.use((req, res) => {
---
+++ routes/blog-routes.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const Blog = require("../models/blog");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("blogs", { titulo: "Blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.post("/", (req, res) => {
+  const blog = new Blog(req.body);
+  console.log(blog);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.get("/novo", (req, res) => {
+  res.render("create", { titulo: "Criar novo Post" });
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("blog-view", { titulo: "Detalhes", blog: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findByIdAndDelete(id)
+    .then((result) => {
+      res.json({ redirect: "/blogs" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
